a40fe4bb095f74d8a80f4b97c54bb9e62f05ea29
BugHunter-X <askzenai@gmail.com>
2024-12-20 03:08:07 +0100

    Add ConcurrentModificationExceptionExample.java file

---
+++ ConcurrentModificationExceptionExample.java
@@ -0,0 +1,12 @@
+public class ConcurrentModificationExceptionExample {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+
+        for (int i : list) {
+            if (i % 2 == 0) {
+                list.remove(Integer.valueOf(i)); // ConcurrentModificationException
+            }
+        }
+        System.out.println(list);
+    }
+}
